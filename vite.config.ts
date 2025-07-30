84d1eb1b53f75ba5af7639e8fd5e777180153636
sekigon-gonnoc <43873124+sekigon-gonnoc@users.noreply.github.com>
2025-07-30 20:04:32 +0900

    Fix title in index.html and update base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/bmp-qmk-zmk-config-converter/",
+  base: "/bmp-qmk-zmk-converter/",
 })
